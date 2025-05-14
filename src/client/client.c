f913f1e14c7f79661d879879187e5f416e62ce53
Gabriele <gmercolino2003@gmail.com>
2025-05-14 12:13:14 +0200

    chore: using parser for args

--- src/client/client.c
+++ src/client/client.c
@@ -1,7 +1,21 @@
+#include "client/args.h"
+
 #include <stdio.h>
 
-int main() {
-  printf("Hello from client\n");
+int main(int argc, char *argv[]) {
+  ClientConfig config = {0};
+  PAResult result = parse_args(argc, argv, &config);
+
+  if (result == ONLY_HELP) {
+    print_usage(argv[0]);
+    return 0;
+  }
+
+  if (result != OK) {
+    fprintf(stderr, "%s\n", pa_result_to_string(result));
+    print_usage(argv[0]);
+    return 1;
+  }
 
   return 0;
 }
