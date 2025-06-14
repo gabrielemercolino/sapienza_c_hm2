55395a6c7eabf6f93964ddb4daffe8b7386c0a2d
Gabriele <gmercolino2003@gmail.com>
2025-06-14 16:47:18 +0200

    chore: sistemata la crittografia

--- src/client/encryption.h
+++ src/client/encryption.h
@@ -2,22 +2,17 @@
 // Created by Pc on 13.06.2025.
 //
 
-#ifndef ENCRYPTION_H
-#define ENCRYPTION_H
-
-#endif //ENCRYPTION_H
-
+#pragma once
 
 #include <stddef.h>
 #include <stdint.h>
 
-
 typedef struct {
-    char *plaintext;
-    char *ciphertext;
-    size_t index;
-    uint64_t key;
+  char *plaintext;
+  char *ciphertext;
+  size_t index;
+  uint64_t key;
 } EncryptTask;
 
-
-char *encrypt_file(const char *filename, uint64_t key, size_t *out_len, size_t threads) ;
+char *encrypt_file(const char *filename, uint64_t key, size_t *out_len,
+                   size_t *padding_len, size_t threads);
