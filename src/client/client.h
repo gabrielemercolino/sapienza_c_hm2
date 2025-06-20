1da6a325e1d863cfac9d000302d7d02cbf911a13
Gabriele <gmercolino2003@gmail.com>
2025-06-20 19:31:45 +0200

    fix: prima non aveva senso

--- src/client/client.h
+++ src/client/client.h
@@ -1,22 +1,11 @@
-//
-// Created by Pc on 13.06.2025.
-//
+#pragma once
 
-#ifndef CLIENT_H
-#define CLIENT_H
 #include <stddef.h>
 #include <stdint.h>
 
-#endif //CLIENT_H
-
-
-
 typedef struct {
-    uint8_t *data;
-    size_t size;
-    uint64_t key;
-    uint8_t *encrypted_data;
+  uint8_t *data;
+  size_t size;
+  uint64_t key;
+  uint8_t *encrypted_data;
 } EncryptionBlock;
-
-
-
