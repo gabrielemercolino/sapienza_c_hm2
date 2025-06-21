611bf78faac4fed858be6b9b4f70a9739bf257d0
Gabriele <gmercolino2003@gmail.com>
2025-06-21 15:40:22 +0200

    refactor: moved encrypted text at the end of the message as it was causing measuring issues

--- src/common/message.h
+++ src/common/message.h
@@ -16,9 +16,9 @@ enum AckType {
 typedef struct {
   size_t original_len;  // Length of the original message
   size_t encrypted_len; // Length of the encrypted message
-  char *encrypted_text; // Pointer to the message data
   uint64_t key;         // Key for encryption/decryption
   size_t threads;       // Number of threads for decryption
+  char *encrypted_text; // Pointer to the message data
 } Message;
 
 /**
