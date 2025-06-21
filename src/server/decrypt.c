53ea9b91ba059f48d36608d531fbe188a74f7cd8
Gabriele <gmercolino2003@gmail.com>
2025-06-21 15:53:46 +0200

    chore: removed prints

--- src/server/decrypt.c
+++ src/server/decrypt.c
@@ -47,9 +47,6 @@ char *decrypt_message(char *ciphertext, size_t padded_len, uint64_t key,
 
   size_t num_blocks = padded_len / BLOCK_SIZE;
 
-  printf("padding_len=%zu\n", padded_len);
-  printf("num_blocks=%zu\n", num_blocks);
-
   ThreadPool *pool = create_thread_pool(n_threads);
 
   // per ogni blocco da 64 bit creo decrypt task e avvio il thread pool
