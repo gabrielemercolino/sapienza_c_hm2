88539cc3673a68302273af458e409a6845f5bfe0
TcY0903 <yangtiancheng2004@gmail.com>
2025-06-23 20:27:51 +0200

    encryption

--- src/client/encryption.c
+++ src/client/encryption.c
@@ -52,16 +52,15 @@ char *encrypt_file(const char *filename, uint64_t key,
     padding = 0;
   }
 
-  size_t padded_bit_len = bit_length + padding;
-  size_t num_blocks = padded_bit_len / BLOCK_SIZE;
-  size_t padded_len = padded_bit_len / 8;
-  *out_len = padded_len;
+  size_t padded_len = bit_length + padding;
+  *out_len = padded_len / 8;
+  size_t num_blocks = padded_len / BLOCK_SIZE;
 
-  char *padded_data = calloc(1, padded_len); // auto padding con '\0'
+  char *padded_data = calloc(1, *out_len); // auto padding con '\0'
   memcpy(padded_data, data, *in_len);
   free(data);
 
-  char *cipherdata = malloc(padded_len);
+  char *cipherdata = malloc(*out_len);
   if (!cipherdata) {
     free(padded_data);
     return NULL;
